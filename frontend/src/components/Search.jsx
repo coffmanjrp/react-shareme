d1eb677d7bf5f8e62ddc0c28ecce3880226d1a57
coffmanjrp <coffmanjrp@gmail.com>
2021-12-28 16:03:23 -0800

    Search & Categories

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from 'react';
+import { MasonryLayout, Spinner } from './';
+import { feedQuery, searchQuery } from 'utils/data';
+import { client } from 'client';
+
 const Search = ({ searchTerm, setSearchTerm }) => {
-  return <div>Search Search</div>;
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+
+      const query = searchQuery(searchTerm.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {loading && <Spinner message="Searching for pins..." />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className="mt-10 text-center text-xl">No Pins Found!</div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
